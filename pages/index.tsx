29e10360697164c974f957515a1fa562cdf36576
Marten de Groot <mdegroot@xebia.com>
2022-11-12 11:38:43 +0100

    Upgrade to Next 13

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import Image from "next/image";
+import Image from "next/legacy/image";
 import styles from "../styles/Home.module.scss";
 import logo from "@/images/logo2.png";
 import verinormLogo from "@/images/verinorm.png";
