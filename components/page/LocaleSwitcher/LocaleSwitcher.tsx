b3cd7a1742590442c89d13d27246669e0e34133c
Marten de Groot <mdegroot@xebia.com>
2022-12-18 16:22:35 +0100

    Fixed LocaleSwitcher

--- components/page/LocaleSwitcher/LocaleSwitcher.tsx
+++ components/page/LocaleSwitcher/LocaleSwitcher.tsx
@@ -1,34 +1,23 @@
-import { useEffect } from "react";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import classnames from "classnames";
 import styles from "./LocaleSwitcher.module.scss";
 
 export const LocaleSwitcher = ({ pageMapping }: { pageMapping?: any }) => {
-  const { locale, locales, asPath, push } = useRouter();
+  const { locale, locales, asPath } = useRouter();
 
-  useEffect(() => {
-    const storedSelection = localStorage.getItem("selectedLocale");
-    if (storedSelection && storedSelection !== locale)
-      push(asPath, undefined, { locale: storedSelection });
-  }, [asPath, locale, push]);
-
-  const storeLocaleSelection = (languageCode: string) => {
-    localStorage.setItem("selectedLocale", languageCode);
-  };
-
-  const redirectPath = pageMapping
-    ? `/${pageMapping[asPath.replace(/\//g, "")]}/`
-    : asPath;
+  const translatePath = (path: string, languageCode: typeof locale): string =>
+    !pageMapping || languageCode === locale
+      ? path
+      : `/${pageMapping[asPath.replace(/\//g, "")]}/`;
 
   return (
     <div className={styles.wrapper}>
       {locales?.map((languageCode) => (
         <Link
           key={languageCode}
-          href={redirectPath}
+          href={translatePath(asPath, languageCode)}
           locale={languageCode}
-          onClick={() => storeLocaleSelection(languageCode)}
         >
           <div
             className={classnames(styles.button, {
